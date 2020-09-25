0f208fa0b372954729ce1c7d58701b0b96127eba
Rafael Leal <rafaelleal.hu@gmail.com>
2020-09-25 17:28:12 -0300

    Refatoramento 1xbet, parimatch e leonbet

--- assets/js/vue.js
+++ assets/js/vue.js
@@ -15,24 +15,24 @@ var app = new Vue({
         authorization:
           "657e0c7308ac4accbedf9887314c4506fb7ff9d7d5d84dcb9d03f9d838086bef",
       },
-      // {
-      //   name: "1xbet",
-      //   url: "https://api.betting-api.com/1xbet/football/live/all",
-      //   authorization:
-      //     "40bd95985ebd433ab18dd64d3880ba4bbc44c33c839f45d6b4b8039bde555dc6",
-      // },
-      // {
-      //   name: "parimatch",
-      //   url: "https://api.betting-api.com/parimatch/football/live/all",
-      //   authorization:
-      //     "a1f2f4afa9024130b70662dfbde48ec6f9e7ddae8042494e970b7fd9753a01d4",
-      // },
-      // {
-      //   name: "leonbets",
-      //   url: "https://api.betting-api.com/leonbets/football/live/all",
-      //   authorization:
-      //     "965d0e44ee5d461abde583481ba9c165f807761c2feb47828549be6513a01e74",
-      // },
+      {
+        name: "1xbet",
+        url: "https://api.betting-api.com/1xbet/football/live/all",
+        authorization:
+          "40bd95985ebd433ab18dd64d3880ba4bbc44c33c839f45d6b4b8039bde555dc6",
+      },
+      {
+        name: "parimatch",
+        url: "https://api.betting-api.com/parimatch/football/live/all",
+        authorization:
+          "a1f2f4afa9024130b70662dfbde48ec6f9e7ddae8042494e970b7fd9753a01d4",
+      },
+      {
+        name: "leonbets",
+        url: "https://api.betting-api.com/leonbets/football/live/all",
+        authorization:
+          "965d0e44ee5d461abde583481ba9c165f807761c2feb47828549be6513a01e74",
+      },
     ],
   },
 
@@ -86,10 +86,10 @@ var app = new Vue({
         obj.url             = item.href;
         obj.liga            = item.league.toLocaleLowerCase();
         obj.equipe_1        = item.team1.replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, " ").toLocaleLowerCase();
-        obj.odd_equipe_1    = (item.markets.win1) ? item.markets.win1.v : '0.00';
+        obj.odd_equipe_1    = (item.markets.win1) ? item.markets.win1.v.toFixed(2) : '0.00';
         obj.equipe_2        = item.team2.replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, " ").toLocaleLowerCase();
-        obj.odd_equipe_2    = (item.markets.win2) ? item.markets.win2.v : '0.00';
-        obj.odd_empate      = (item.markets.winX) ? item.markets.winX.v : '0.00';
+        obj.odd_equipe_2    = (item.markets.win2) ? item.markets.win2.v.toFixed(2) : '0.00';
+        obj.odd_empate      = (item.markets.winX) ? item.markets.winX.v.toFixed(2) : '0.00';
 
         this.matchesGGbet.push(obj);
       });
@@ -101,38 +101,55 @@ var app = new Vue({
      *  Monta as partidas da 1XBET
      */
     get1xbet(response) {
-      response.body.forEach((i) => {
-        if (i.markets.win1 && i.markets.win2 && i.markets.winX) {
-          this.matches1xbet.push([
-            new Date(i.date_start).toLocaleString(),
-            i.href,
-            i.league.name.toLocaleLowerCase(),
-            i.team1
-              .replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, " ")
-              .toLocaleLowerCase(),
-            i.markets.win1.v,
-            i.team2
-              .replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, " ")
-              .toLocaleLowerCase(),
-            i.markets.win2.v,
-            i.markets.winX.v,
-          ]);
-        } else {
-          this.matches1xbet.push([
-            new Date(i.date_start).toLocaleString(),
-            i.href,
-            i.league.name.toLocaleLowerCase(),
-            i.team1
-              .replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, " ")
-              .toLocaleLowerCase(),
-            0,
-            i.team2
-              .replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, " ")
-              .toLocaleLowerCase(),
-            0,
-            0,
-          ]);
-        }
+      response.body.forEach((item) => {
+        let obj = {};
+
+        obj.data            = new Date(item.date_start).toLocaleString();
+        obj.url             = item.href;
+        obj.liga            = item.league.name.toLocaleLowerCase();
+        obj.equipe_1        = item.team1.replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, " ").toLocaleLowerCase();
+        obj.odd_equipe_1    = (item.markets.win1) ? item.markets.win1.v.toFixed(2) : '0.00';
+        obj.equipe_2        = item.team2.replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, " ").toLocaleLowerCase();
+        obj.odd_equipe_2    = (item.markets.win2) ? item.markets.win2.v.toFixed(2) : '0.00';
+        obj.odd_empate      = (item.markets.winX) ? item.markets.winX.v.toFixed(2) : '0.00';
+
+        this.matches1xbet.push(obj);
+
+
+
+
+
+        // if (i.markets.win1 && i.markets.win2 && i.markets.winX) {
+        //   this.matches1xbet.push([
+        //     new Date(i.date_start).toLocaleString(),
+        //     i.href,
+        //     i.league.name.toLocaleLowerCase(),
+        //     i.team1
+        //       .replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, " ")
+        //       .toLocaleLowerCase(),
+        //     i.markets.win1.v,
+        //     i.team2
+        //       .replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, " ")
+        //       .toLocaleLowerCase(),
+        //     i.markets.win2.v,
+        //     i.markets.winX.v,
+        //   ]);
+        // } else {
+        //   this.matches1xbet.push([
+        //     new Date(i.date_start).toLocaleString(),
+        //     i.href,
+        //     i.league.name.toLocaleLowerCase(),
+        //     i.team1
+        //       .replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, " ")
+        //       .toLocaleLowerCase(),
+        //     0,
+        //     i.team2
+        //       .replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, " ")
+        //       .toLocaleLowerCase(),
+        //     0,
+        //     0,
+        //   ]);
+        // }
       });
 
       console.log("terminou a segunda");
@@ -142,38 +159,54 @@ var app = new Vue({
      *  monta as partidas da PARIMATCH
      */
     getPariMatch(response) {
-      response.body.forEach((ix) => {
-        if (ix.win1 && ix.win2 && ix.winX) {
-          this.matchesPari.push([
-            new Date(ix.actual_at).toLocaleString(),
-            ix.href,
-            ix.title.toLocaleLowerCase(),
-            ix.team1
-              .replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, " ")
-              .toLocaleLowerCase(),
-            ix.win1.value,
-            ix.team2
-              .replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, " ")
-              .toLocaleLowerCase(),
-            ix.win2.value,
-            ix.winX.value,
-          ]);
-        } else {
-          this.matchesPari.push([
-            new Date(ix.actual_at).toLocaleString(),
-            ix.href,
-            ix.title.toLocaleLowerCase(),
-            ix.team1
-              .replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, " ")
-              .toLocaleLowerCase(),
-            0,
-            ix.team2
-              .replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, " ")
-              .toLocaleLowerCase(),
-            0,
-            0,
-          ]);
-        }
+      response.body.forEach((item) => {
+
+        let obj = {};
+
+        obj.data            = new Date(item.actual_at).toLocaleString();
+        obj.url             = item.href;
+        obj.liga            = item.title.toLocaleLowerCase();
+        obj.equipe_1        = item.team1.replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, " ").toLocaleLowerCase();
+        obj.odd_equipe_1    = (item.win1) ? item.win1.value.toFixed(2) : '0.00';
+        obj.equipe_2        = item.team2.replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, " ").toLocaleLowerCase();
+        obj.odd_equipe_2    = (item.win2) ? item.win2.value.toFixed(2) : '0.00';
+        obj.odd_empate      = (item.winX) ? item.winX.value.toFixed(2) : '0.00';
+
+        this.matchesPari.push(obj);
+
+
+
+        // if (ix.win1 && ix.win2 && ix.winX) {
+        //   this.matchesPari.push([
+        //     new Date(ix.actual_at).toLocaleString(),
+        //     ix.href,
+        //     ix.title.toLocaleLowerCase(),
+        //     ix.team1
+        //       .replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, " ")
+        //       .toLocaleLowerCase(),
+        //     ix.win1.value,
+        //     ix.team2
+        //       .replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, " ")
+        //       .toLocaleLowerCase(),
+        //     ix.win2.value,
+        //     ix.winX.value,
+        //   ]);
+        // } else {
+        //   this.matchesPari.push([
+        //     new Date(ix.actual_at).toLocaleString(),
+        //     ix.href,
+        //     ix.title.toLocaleLowerCase(),
+        //     ix.team1
+        //       .replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, " ")
+        //       .toLocaleLowerCase(),
+        //     0,
+        //     ix.team2
+        //       .replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, " ")
+        //       .toLocaleLowerCase(),
+        //     0,
+        //     0,
+        //   ]);
+        // }
       });
 
       console.log("terminou a terceira");
@@ -183,38 +216,53 @@ var app = new Vue({
      *  monta as partidas da LEONBETS
      */
     getLeonbets(response) {
-      response.body.forEach((ixt) => {
-        if (ixt.win1 && ixt.win2 && ixt.winX) {
-          this.matchesLeon.push([
-            new Date(ixt.date_start).toLocaleString(),
-            ixt.href,
-            ixt.country.toLocaleLowerCase(),
-            ixt.team1
-              .replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, " ")
-              .toLocaleLowerCase(),
-            ixt.win1.value,
-            ixt.team2
-              .replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, " ")
-              .toLocaleLowerCase(),
-            ixt.win2.value,
-            ixt.winX.value,
-          ]);
-        } else {
-          this.matchesLeon.push([
-            new Date(ixt.date_start).toLocaleString(),
-            ixt.href,
-            ixt.country.toLocaleLowerCase(),
-            ixt.team1
-              .replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, " ")
-              .toLocaleLowerCase(),
-            0,
-            ixt.team2
-              .replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, " ")
-              .toLocaleLowerCase(),
-            0,
-            0,
-          ]);
-        }
+      response.body.forEach((item) => {
+        let obj = {};
+
+        obj.data            = new Date(item.date_start).toLocaleString();
+        obj.url             = item.href;
+        obj.liga            = item.country.toLocaleLowerCase();
+        obj.equipe_1        = item.team1.replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, " ").toLocaleLowerCase();
+        obj.odd_equipe_1    = (item.win1) ? item.win1.value.toFixed(2) : '0.00';
+        obj.equipe_2        = item.team2.replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, " ").toLocaleLowerCase();
+        obj.odd_equipe_2    = (item.win2) ? item.win2.value.toFixed(2) : '0.00';
+        obj.odd_empate      = (item.winX) ? item.winX.value.toFixed(2) : '0.00';
+
+        this.matchesLeon.push(obj);
+
+
+
+        // if (ixt.win1 && ixt.win2 && ixt.winX) {
+        //   this.matchesLeon.push([
+        //     new Date(ixt.date_start).toLocaleString(),
+        //     ixt.href,
+        //     ixt.country.toLocaleLowerCase(),
+        //     ixt.team1
+        //       .replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, " ")
+        //       .toLocaleLowerCase(),
+        //     ixt.win1.value,
+        //     ixt.team2
+        //       .replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, " ")
+        //       .toLocaleLowerCase(),
+        //     ixt.win2.value,
+        //     ixt.winX.value,
+        //   ]);
+        // } else {
+        //   this.matchesLeon.push([
+        //     new Date(ixt.date_start).toLocaleString(),
+        //     ixt.href,
+        //     ixt.country.toLocaleLowerCase(),
+        //     ixt.team1
+        //       .replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, " ")
+        //       .toLocaleLowerCase(),
+        //     0,
+        //     ixt.team2
+        //       .replace(/([\u0300-\u036f]|[^0-9a-zA-Z])/g, " ")
+        //       .toLocaleLowerCase(),
+        //     0,
+        //     0,
+        //   ]);
+        // }
       });
       console.log("terminou a quarta");
     },
@@ -235,16 +283,19 @@ var app = new Vue({
 
         this.maiorOddPartidas.push(objeto);
       });
+
+    },
+    retornoStake1 (stake, odd) {
+      let lucro = stake * odd;
+      return lucro.toFixed(2);
     },
 
-    oddmaior() {
-      this.matchesGGbet.forEach((ggbet) => {
-        this.matches1xbet.forEach((xbet) => {
-          this.matchesPari.forEach((pari) => {
-            this.matchesLeon.forEach((leon) => {});
-          });
-        });
-      });
+    buscarOdd (stake, odd) { 
+    
+      let oddBuscada = (stake*odd) / ((stake*odd) - stake);
+      return oddBuscada.toFixed(2);
+
     },
+
   },
 });
